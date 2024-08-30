e8ddd105ff725c8e11ae348bef7db67d41677f54
Bril3d <khaledtbk@gmail.com>
2024-08-30 23:02:00 +0100

    Code Optimization

--- LeetcodeScraper.py
+++ LeetcodeScraper.py
@@ -1,8 +1,11 @@
 import json
 import os
 import logging
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from concurrent.futures import ThreadPoolExecutor
 from utils.utils import (
     get_element_by_xpath,
     get_elements_by_selector,
@@ -19,16 +22,17 @@ SCRAPER_SUBMITTED_CODE_NAME_XPATH = "/html/body/div[2]/div/div[1]/div/div[1]/h4/
 SUBMISSION_URL_TEMPLATE = "https://leetcode.com/submissions/detail/{}"
 PAGE_URL_TEMPLATE = "https://leetcode.com/submissions/#/{}"
 
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 class LeetcodeScraper:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
         login(self.driver)
+        self.session = requests.Session()
+        for cookie in self.driver.get_cookies():
+            self.session.cookies.set(cookie['name'], cookie['value'])
 
     def save_scraped_solution(self, file_content):
         os.makedirs("./problems", exist_ok=True)
@@ -36,31 +40,20 @@ class LeetcodeScraper:
             json.dump(file_content, f)
 
     def scrape_and_save_code_from_submission_id(self, submission_id):
-        self.driver.get(SUBMISSION_URL_TEMPLATE.format(submission_id))
+        url = SUBMISSION_URL_TEMPLATE.format(submission_id)
+        response = self.session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
         try:
-            name_div = get_element_by_xpath(
-                self.driver, SCRAPER_SUBMITTED_CODE_NAME_XPATH
-            )
-            href = name_div.get_attribute("href")
+            name_div = soup.select_one(SCRAPER_SUBMITTED_CODE_NAME_XPATH)
+            href = name_div['href']
             name_div_value = href.split("/")[-2]
 
-            language_div = get_element_by_xpath(
-                self.driver, SCRAPER_SUBMITTED_CODE_LANGUAGE_XPATH
-            )
+            language_div = soup.select_one(SCRAPER_SUBMITTED_CODE_LANGUAGE_XPATH)
             language_div_value = language_div.text
 
-            code_div = get_element_by_xpath(
-                self.driver, SCRAPER_SUBMITTED_CODE_DIV_XPATH, 10
-            )
-
-            actions = ActionChains(self.driver)
-            actions.move_to_element(code_div).click().perform()
-
-            select_all_and_copy(self.driver)
-
-            copied_text = paste()
-
+            code_div = soup.select_one(SCRAPER_SUBMITTED_CODE_DIV_XPATH)
+            copied_text = code_div.text
             file_content = {
                 "problemName": name_div_value,
                 "language": language_div_value,
@@ -72,38 +65,38 @@ class LeetcodeScraper:
             logger.error(f"Error while scraping submission ID {submission_id}: {e}")
 
     def scrape_submission_ids_from_page_id(self, page_id):
-        self.driver.get(PAGE_URL_TEMPLATE.format(page_id))
+        url = PAGE_URL_TEMPLATE.format(page_id)
+        response = self.session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
         try:
-            rows = get_elements_by_selector(self.driver, ".text-success")
-            return [row.get_attribute("href").split("/")[-2] for row in rows]
-
+            rows = soup.select(".text-success")
+            return [row['href'].split("/")[-2] for row in rows]
         except Exception as e:
-            logger.error(
-                f"Error while scraping submission IDs from page {page_id}: {e}"
-            )
+            logger.error(f"Error while scraping submission IDs from page {page_id}: {e}")
             return []
-
     def scrape_code_from_all_submissions(self):
-        for page_no in range(1, 1001):
-            try:
-                submission_ids = self.scrape_submission_ids_from_page_id(page_no)
-                logger.info(
-                    f"Submission IDs fetched from page number {page_no}: {submission_ids}"
-                )
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            for page_no in range(1, 1001):
+                try:
+                    submission_ids = self.scrape_submission_ids_from_page_id(page_no)
+                    logger.info(f"Submission IDs fetched from page number {page_no}: {submission_ids}")
+                    if not submission_ids:
+                        break
 
-                for submission_id in submission_ids:
-                    self.scrape_and_save_code_from_submission_id(submission_id)
+                    futures = [executor.submit(self.scrape_and_save_code_from_submission_id, submission_id) 
+                               for submission_id in submission_ids]
 
-            except Exception as e:
-                logger.error(f"Error in page {page_no}: {e}")
-                break
+                    for future in futures:
+                        future.result()
 
+                except Exception as e:
+                    logger.error(f"Error in page {page_no}: {e}")
+                    break
     def scrape_accepted_solutions(self):
         logger.info("<<<< Starting Leetcode Scraper >>>>")
         self.scrape_code_from_all_submissions()
         logger.info("<<<< Exiting Leetcode Scraper >>>>")
-
 
 if __name__ == "__main__":
     scraper = LeetcodeScraper()
